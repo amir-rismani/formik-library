2aa40ce8029b9238b9c4b148924acab7a1123e54
Amir Rismaani <amir.rismaani@gmail.com>
2023-06-08 05:01:21 +0330

    form ui and state managment with formik

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 
+import { useFormik } from 'formik';
 import './App.css';
+const initialValues = {
+  name: "",
+  email: "",
+  password: ""
+}
 function App() {
+  const formik = useFormik({ initialValues })
+  console.log(formik);
   return (
     <div className="App">
-
+      <h1>Signup</h1>
+      <form>
+        <div className='formGroup'>
+          <label htmlFor='name'>Name</label>
+          <input value={formik.values.name} name='name' onChange={formik.handleChange} />
+        </div>
+        <div className='formGroup'>
+          <label htmlFor='email'>Email</label>
+          <input value={formik.values.email} name='email' onChange={formik.handleChange} />
+        </div>
+        <div className='formGroup'>
+          <label htmlFor='password'>Password</label>
+          <input value={formik.values.password} name='password' onChange={formik.handleChange} />
+        </div>
+        <button type='submit'>Register</button>
+      </form>
     </div>
   );
 }
