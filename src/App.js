6c72b2ccd00675597e8efc4c3a3fd17fcf6d6cce
Amir Rismaani <amir.rismaani@gmail.com>
2023-06-11 10:31:12 +0330

    add radio button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const initialValues = {
   phone: "",
   password: "",
   passwordConfirmation: "",
+  gender: ""
 }
 
 // 2. Handle submittion
@@ -33,7 +34,8 @@ const validationSchema = Yup.object({
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
     "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
   ),
-  passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match')
+  passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match'),
+  gender: Yup.string().required()
 });
 function App() {
   const formik = useFormik({ initialValues, onSubmit, validationSchema, validateOnMount: true })
@@ -43,30 +45,36 @@ function App() {
       <form onSubmit={formik.handleSubmit}>
         <div className='formGroup'>
           <label htmlFor='name'>Name</label>
-          <input name='name' {...formik.getFieldProps('name')} />
+          <input type='text' id='name' name='name' {...formik.getFieldProps('name')} />
           {formik.errors.name && formik.touched.name && <small className='error'>{formik.errors.name}</small>}
         </div>
         <div className='formGroup'>
           <label htmlFor='email'>Email</label>
-          <input name='email' {...formik.getFieldProps('email')} />
+          <input type='email' id='email' name='email' {...formik.getFieldProps('email')} />
           {formik.errors.email && formik.touched.email && <small className='error'>{formik.errors.email}</small>}
         </div>
         <div className='formGroup'>
           <label htmlFor='phone'>Phone</label>
-          <input name='phone' {...formik.getFieldProps('phone')} />
+          <input type='text' id='phone' name='phone' {...formik.getFieldProps('phone')} />
           {formik.errors.phone && formik.touched.phone && <small className='error'>{formik.errors.phone}</small>}
         </div>
         <div className='formGroup'>
           <label htmlFor='password'>Password</label>
-          <input name='password' {...formik.getFieldProps('password')} />
+          <input type='password' id='password' name='password' {...formik.getFieldProps('password')} />
           {formik.errors.password && formik.touched.password && <small className='error'>{formik.errors.password}</small>}
         </div>
         <div className='formGroup'>
           <label htmlFor='passwordConfirmation'>Password Confirmation</label>
-          <input name='passwordConfirmation' {...formik.getFieldProps('passwordConfirmation')} />
+          <input type='password' id='passwordConfirmation' name='passwordConfirmation' {...formik.getFieldProps('passwordConfirmation')} />
           {formik.errors.passwordConfirmation && formik.touched.passwordConfirmation && <small className='error'>{formik.errors.passwordConfirmation}</small>}
         </div>
-
+        <div className='formGroup'>
+          <input type='radio' id="male" name='gender' value="mail" onChange={formik.handleChange} />
+          <label htmlFor='male'>Male</label>
+          <input type='radio' id="female" name='gender' value="femail" onChange={formik.handleChange} />
+          <label htmlFor='female'>Female</label>
+          {formik.errors.gender && formik.touched.gender && <small className='error'>{formik.errors.gender}</small>}
+        </div>
         <button type='submit' disabled={!formik.isValid}>Register</button>
       </form>
     </div>
